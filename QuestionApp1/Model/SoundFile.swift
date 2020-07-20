3c3533377ad7c0844458c4200a3856c8eb9f88b8
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-07-20 23:22:10 +0900

    create SoundFile's Model.

---
+++ QuestionApp1/Model/SoundFile.swift
@@ -0,0 +1,21 @@
+//
+//  SoundFile.swift
+//  QuestionApp1
+//
+//  Created by 野口晃 on 2020/07/20.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import Foundation
+import  AVFoundation
+
+class SoundFile{
+    
+    var player:AVAudioPlayer?
+    
+    func playSound(fileName:String,extensionName:String){
+        
+        
+       //再生する
+    }
+}
